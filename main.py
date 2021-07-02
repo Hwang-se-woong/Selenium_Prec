203fecfc6020c213f58f8d4097d00103a15e11dc
user <ghkdto4633>
2021-07-02 15:32:10 +0900

    Add login function

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
+import private_info
 
 driver = webdriver.Chrome()
-driver.get('https://www.naver.com/')
+# driver.get('https://www.naver.com/')
+driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com')
+
+driver.find_element_by_name('id').send_keys(private_info.id)
+driver.find_element_by_name('pw').send_keys(private_info.pw)
+driver.find_element_by_xpath('//*[@id="log.login"]').click()
+
